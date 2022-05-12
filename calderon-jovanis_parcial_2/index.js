1e062983d70b04e7dc8ab73dce0f97a47eeee15e
Jovanis J. Calderon <jcalderon@bandelta.com>
2022-05-11 22:19:46 -0500

    Parte 2

--- calderon-jovanis_parcial_2/index.js
+++ calderon-jovanis_parcial_2/index.js
@@ -4,15 +4,13 @@
             pokemonFinderForm: document.querySelector("#pokemon-finder-form"),
             pokemonFinderInput: document.querySelector("#pokemon-finder-query"),
             pokemonFinderSearchType: document.querySelector("#pokemon-finder-search-type"),
+            pokemonFinderOutput: document.querySelector("pokemon-finder-response")
         },
         init: () => {
             App.htmlElements.pokemonFinderForm.addEventListener(
                 "submit",
                 App.handlers.pokemonFinderFormOnSubmit
             );
-        },
-        templates: {
-
         },
         handlers: {
             pokemonFinderFormOnSubmit: async (e) => {
@@ -21,8 +19,28 @@
                 const query = App.htmlElements.pokemonFinderInput.value;
                 const searchType = App.htmlElements.pokemonFinderSearchType.value;
 
-                const response = await Utils.getPokemon({query, searchType});
-                console.log(response)
+                try {
+                    const response = await Utils.getPokemon({query, searchType});
+                    console.log(response)
+                } catch(error) {
+                    App.htmlElements.pokemonFinderOutput.innerHTML = `<h1>${error}</h1>`;
+                }
+            }
+        },
+        templates: {
+            render: ({searchType, response}) => {
+                const renderMap = {
+                    ability: App.templates.abilityCard,
+                    pokemon: App.templates.pokemonCard
+                };
+                return renderMap[searchType] ? renderMap[searchType](response): App.templates.errorCard();
+            },
+            errorCard: () => `<h1>There was an error</h1>`,
+            pokemonCard: ({id, name, weight, height}) => {
+
+            },
+            abilityCard: ({id, name, pokemon}) => {
+
             }
         }
     }
